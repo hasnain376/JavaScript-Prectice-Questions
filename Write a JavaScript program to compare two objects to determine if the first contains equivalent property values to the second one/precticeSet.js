0f095c10267caa70ca63b6ba5c8f19c53d0fcf57
Muhammad Hasnain <m.hasnain986786@gmail.com>
2024-12-28 22:40:36 +0500

    Add files via upload

---
+++ Write a JavaScript program to compare two objects to determine if the first contains equivalent property values to the second one/precticeSet.js
@@ -0,0 +1,30 @@
+// const obj1 = {
+//     name : "Muhammad Hasnain",
+//     age : 25,
+//     city : "Karachi"
+// }
+//
+// const source = {
+//     name : "Muhammad Hasnain",
+//     age : 25,
+//     city : "Karachi"
+// }
+//
+// function checkEquivalentProperties(obj , sources) {
+//     return Object.keys(sources).every(key => obj.hasOwnProperty(key) && obj[key] === sources[key]);
+//
+//
+// }
+//
+// checkEquivalentProperties({name:"Muhammad Hasnain",age : 25, city : "Karachi"},
+//     {name: "Muhammad Hasnain" , age : 25, city : "Karachi" , home: "malaria"});
+
+//#Source https://bit.ly/2neWfJ2
+// Define a function called `matches` that takes two objects as arguments and checks if the first object contains all the key-value pairs of the second object.
+const matches = (obj, source) =>
+    // Iterate over each key in the source object and check if it exists in the obj object and has the same value.
+    Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
+// Test cases:
+console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
+console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
+console.log(matches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
